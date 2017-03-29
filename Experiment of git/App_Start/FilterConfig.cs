543dc9443614c44504ee452ed2bb820f9fff897d
desai.suraj35 <desai.suraj35@gmail.com>
2017-03-30 04:47:36 +1100

    Add project files.

---
+++ Experiment of git/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Experiment_of_git
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Experiment of git/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Experiment_of_git.MvcApplication" Language="C#" %>
